e47dfad65725f4a652529ac45d3987a36ec1bd32
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-23 19:14:49 +0000

    displayed data on the frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { prisma } from '@/lib/prisma'
+import { userAgentFromString } from 'next/server';
 
-export default function Home() {
+export default async function Home() {
+  let students = await prisma.student.findMany();
+  
   return (
-    <h1>Hello World</h1>
-  )
+    <>
+      <h1>Students</h1>
+      {students.map(student => {
+        return (<ul>
+          <li>{student.fullname}</li>
+          <li>{student.age}</li>
+          <li>{student.course}</li>
+          </ul>
+        )})}
+    </>
+  );
 }
